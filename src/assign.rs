f342c778c31ba849a8c1d55f775a66650cb56bdb
attesnn <attesnn@gmail.com>
2025-03-23 16:50:32 +0200

    new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/assign.rs 	new file:   src/main.rs

---
+++ src/assign.rs
@@ -0,0 +1,19 @@
+use std::io;
+    pub fn assign_project_number () -> u32 {
+        loop {
+            let mut proj_num = String::new();    
+                println!("Please input your Project Number. It must be an integer.");
+         
+                io::stdin()
+                    .read_line(&mut proj_num)
+                    .expect("failed to read line");
+                 
+                let proj_num: u32 = match proj_num.trim().parse() {
+                    Ok(num) => num,            
+                    Err(_) => continue,
+                };
+                
+                println!("You project number is: {proj_num}");
+                return proj_num;
+        };
+    }
